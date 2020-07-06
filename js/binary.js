89a16ac3c012470db3da23013a4a26160782ba4e
oskar-binary <oskar@binary.com>
2020-07-06 16:15:21 +0800

    Deploy to gh-pages

--- js/binary.js
+++ js/binary.js
@@ -1272,16 +1272,14 @@ module.exports = GTM;
 "use strict";
 
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var ClientBase = __webpack_require__(/*! ./client_base */ "./src/javascript/_common/base/client_base.js");
 
 var LiveChat = function () {
 
     var init = function init() {
         if (window.LiveChatWidget) {
-            // window.LiveChatWidget.on('ready', () => {
-            //     window.LiveChatWidget.call('set_customer_name', 'John Doe');
-            //     // window.LiveChatWidget.call('set_customer_email', authorize.email);
-            // });
             window.LiveChatWidget.on('visibility_changed', function (_ref) {
                 var visibility = _ref.visibility;
 
@@ -1291,6 +1289,21 @@ var LiveChat = function () {
 
                     if (email) window.LiveChatWidget.call('set_customer_email', email);
                     if (fullname) window.LiveChatWidget.call('set_customer_name', fullname);
+
+                    // only visible to CS
+                    var loginid = ClientBase.get('loginid');
+                    var landing_company_shortcode = ClientBase.get('landing_company_shortcode');
+                    var currency = ClientBase.get('currency');
+                    var residence = ClientBase.get('residence');
+
+                    var session_variables = _extends({}, loginid && { loginid: loginid }, landing_company_shortcode && { landing_company_shortcode: landing_company_shortcode }, currency && { currency: currency }, residence && { residence: residence });
+
+                    window.LiveChatWidget.call('set_session_variables', session_variables);
+                }
+
+                if (visibility === 'maximized' && !ClientBase.isLoggedIn()) {
+                    window.LiveChatWidget.call('set_customer_email', '');
+                    window.LiveChatWidget.call('set_customer_name', '');
                 }
             });
         }
